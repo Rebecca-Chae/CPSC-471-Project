c6a4319474bf75781e4c000268f5767d2b7a8f34
HaiderTawfik <87101270+HaiderTawfik@users.noreply.github.com>
2022-11-22 21:09:27 -0700

    Update border for table, begin adding query

--- Professional.php
+++ Professional.php
@@ -1,20 +1,28 @@
 <?php
-$connection = mysqli_connect("", "", "", "");
+$connection = mysqli_connect("localhost", "root", "Password", "fitnessTracker");
 if (mysqli_connect_errno($con))
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
-  
  
  if (!mysqli_query($con,$sql))
   {
   //die('Error: e' . mysqli_error($con));
   }
+  $q = "SELECT * FROM Appointments WHERE Professional_Username = $.username";
+  $Appoints = $connection -> query($q);
+  $q = "SELECT * FROM  hires WHERE Professional_Username = $.username";
+  $Clients = $connection -> query($q);
 ?>
 
 <html>
     <body>
-        <div id = "usernae">
+      <style>
+            table, th, td {
+              border: 1px solid black;
+            }
+          </style>
+        <div id = "username">
           <h1>NAME_OF_PROFESSIONAL</h1>
         </div> 
         <div style = "text-align: right">
@@ -25,10 +33,11 @@ if (mysqli_connect_errno($con))
           Your next Scheduled Appointment is on ___ with __
         </div>
         <div id = "Friends">
-          <table style = "float: right">
+          <table style = "float: right" bordercolor = black>
             <tr>
               <th>Friends</th>
             </tr>
+            <tr><td>Temp test</tr></td>
             <tr>
               <td>
                 <button id = "addFriends"> Add Friends </button>
@@ -37,7 +46,7 @@ if (mysqli_connect_errno($con))
           </table>
         </div>
         <div id = "clients">
-          <table style = "float: right;" WIDTH = 200>
+          <table style = "float: right;" WIDTH = 200 bordercolor = black>
             <tr>
               <th>Clients</th>
             </tr>
